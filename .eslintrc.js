5462fd027236e3aa5daec8f0d5be69fd396ff912
Miguel Ángel Barquilla <miguelangelbarquilla@gmail.com>
2022-10-10 20:33:30 +0200

    Remove BnB of the .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,7 +6,7 @@ module.exports = {
   extends: [
     "eslint:recommended",
     "plugin:@typescript-eslint/recommended",
-    ["airbnb-base", "prettier"],
+    "prettier",
   ],
   overrides: [],
   parser: "@typescript-eslint/parser",
